f305ea272933de6f7fec6a23cea0101605283a24
cah-friat-hadush <friat.hadush@cardinalhealth.com>
2021-10-24 21:48:58 -0400

    [first-commit]

---
+++ MiscellaneousSideProjects/src/com/leetcode/exercises/FAANG.java
@@ -0,0 +1,11 @@
+package com.leetcode.exercises;
+
+/**
+ * @Author friat.hadush
+ * @Create 9:39 PM
+ */
+public class FAANG {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
